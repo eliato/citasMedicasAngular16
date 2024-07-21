21d8991c755e096a87217b1df16b02f464a2ed5c
Elias Torres <eliato_@hotmail.com>
2024-07-21 13:21:25 -0600

    login

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth/auth.service';
 import { routes } from 'src/app/shared/routes/routes';
 
@@ -17,14 +18,14 @@ export class LoginComponent implements OnInit {
       Validators.required,
       Validators.email,
     ]),
-    password: new FormControl('123456', [Validators.required]),
+    password: new FormControl('12345678', [Validators.required]),
   });
 
   get f() {
     return this.form.controls;
   }
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, public router: Router) {}
   ngOnInit(): void {
     if (localStorage.getItem('authenticated')) {
       localStorage.removeItem('authenticated');
@@ -33,7 +34,19 @@ export class LoginComponent implements OnInit {
 
   loginFormSubmit() {
     if (this.form.valid) {
-      this.auth.login();
+      this.auth.login(this.form.value.email, this.form.value.password)
+      .subscribe((resp) => {
+        console.log(resp); 
+        if (resp) {
+            //login exitoso
+            this.router.navigate([routes.adminDashboard]);
+        }else{
+            //login fallo
+           alert('usuario no es correcto')
+        }
+      }, error => {
+        console.error(error);
+      } ) 
     }
   }
   togglePassword() {
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,17 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { catchError, map, of } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 // import { BehaviorSubject } from 'rxjs';
-import { routes } from '../routes/routes';
+//import { routes } from '../routes/routes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public http: HttpClient) {}
 
-  public login(): void {
-    localStorage.setItem('authenticated', 'true');
-    this.router.navigate([routes.adminDashboard]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public login(email?: string | null | undefined, password?: string | null | undefined) {
+    // localStorage.setItem('authenticated', 'true');
+    // this.router.navigate([routes.adminDashboard]);
+    const URL = environment.URL_SERVICIOS+"auth/login";
+    return this.http.post(URL, {email,password}).pipe(
+        map( (auth) => {
+            console.log(auth);
+          const result =  this.saveLocalStorage(auth);
+          return result; 
+        } ), catchError( (error) => {
+            console.log(error);
+            return of(undefined);
+        } )
+    )
+  }
+
+  saveLocalStorage(auth:any){
+    if(auth && auth.access_token){
+        localStorage.setItem('token', auth.access_token);
+        localStorage.setItem('user',JSON.stringify(auth.user));
+        localStorage.setItem('authenticated', 'true');
+        return true;
+    }
+    return false;
   }
 }
