beef9f61373364b38ec768ae4bf3586081cb8307
Elias Torres <eliato_@hotmail.com>
2024-07-21 11:59:55 -0600

    add environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+ export const environment = {
+
+    URL_BACKEND:   'http://127.0.0.1:8000',
+    URL_SERVICIOS: 'http://127.0.0.1:8000/api',
+    URL_FRONTEND:  'http://127.0.0.1:4200/'
+
+ };
